66fe2c0192ac0a2b631c649c4f2748e0e81d4262
GustavoTorelli <gustavo.torelli17@gmail.com>
2026-09-09 20:08:08 -0300

    feat: GetUsersController adiconado

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import express, { type Express, type Request, type Response } from 'express';
+import express, { type Express, type Request, type Response } from "express";
+import { config } from "dotenv";
+
+config();
 
 const app: Express = express();
-const port: number = 3001;
+
+const port = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send('Hello World!')
+  res.status(200).send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`)
-})
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
