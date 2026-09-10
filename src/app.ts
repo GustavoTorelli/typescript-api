5883071feebfd15c38e85813e53fe3d8eacff783
GustavoTorelli <gustavo.torelli17@gmail.com>
2026-09-09 21:19:25 -0300

    feat:  MongoDB adicionado e conectado ao mongo client

--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,28 @@ import express, { type Express, type Request, type Response } from "express";
 import { config } from "dotenv";
 import { GetUsersController } from "./controllers/get-users/get-users.js";
 import { MongoGetUsersRepository } from "./repo/get-users/mongo-get-users.js";
+import { MongoClient } from "./database/mongo.js";
 
-config();
+const main = async () => {
+  config();
+  const app: Express = express();
 
-const app: Express = express();
+  await MongoClient.connect();
 
-const port = process.env.PORT || 8000;
+  app.get("/users", async (req: Request, res: Response) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
 
-app.get("/users", async (req: Request, res: Response) => {
-  const mongoGetUsersRepository = new MongoGetUsersRepository();
-  const getUsersController = new GetUsersController(mongoGetUsersRepository);
+    const { body, statusCode } = await getUsersController.handle();
 
-  const {body, statusCode} = await  getUsersController.handle()
+    res.send(body).status(statusCode);
+  });
 
-  res.send(body).status(statusCode)
+  const port = process.env.PORT || 8000;
 
-});
+  app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+  });
+};
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+  main();
