18fb6fcbc212d4362a7d4c6a6908b25bd11a18a2
GustavoTorelli <gustavo.torelli17@gmail.com>
2026-09-12 00:39:36 -0300

    refactor: httpRequest tipado corretamente, novos enums adicionados no httpStatusCode.

--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -4,8 +4,10 @@ export interface HttpResponse<T> {
 }
 
 export interface HttpRequest<B> {
-  params?: any;
-  headers?: any;
+  params?: {
+    id?: string;
+  };
+  headers?: unknown;
   body?: B;
 }
 
@@ -13,6 +15,9 @@ export enum HttpStatusCode {
   OK = 200,
   CREATED = 201,
   BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
   SERVER_ERROR = 500,
 }
 
